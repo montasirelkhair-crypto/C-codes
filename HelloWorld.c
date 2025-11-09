449ae94996a906d9be3425ac35c88b240799a158
Montasir Elsadig Elkhair Ibrahim <montasirelkhair@gmail.com>
2025-11-09 21:39:49 +0000

    learning C

---
+++ HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Hello, Wold!");
+    printf("I am learning\n");
+    return 0; //if code = 0 then program ran successfully, if not then there was an error
+    }

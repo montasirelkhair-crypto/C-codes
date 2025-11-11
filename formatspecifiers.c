ea64d064b5f367ed5887a9b6c6ce290a46c12580
Montasir Elsadig Elkhair Ibrahim <montasirelkhair@gmail.com>
2025-11-11 20:55:05 +0000

    learning c

---
+++ formatspecifiers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () {
+
+    //format specifiers % are used to specify the type of data to be printed or read.
+    /*
+    Here is a list of common format specifiers in C:
+    %d or %i - Integer (decimal)
+    %u - Unsigned integer
+    %f - Floating point number
+    %lf - Double floating point number
+    %c - Character
+    %s - String
+    %x - Hexadecimal integer
+    %o - Octal integer
+    %% - Percent sign   
+    %p - Pointer address
+    %.1 - Floating point number with 1 digit after the decimal point
+    %1 - Minimum field width of 1
+    %- - Left-justify within the given field width
+    and many more.
+    */
+    
+}

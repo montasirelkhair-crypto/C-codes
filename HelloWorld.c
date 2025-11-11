557f96d278ae7178e5c85025ac11364bdaae35f3
Montasir Elsadig Elkhair Ibrahim <montasirelkhair@gmail.com>
2025-11-11 20:09:26 +0000

    learning c

--- HelloWorld.c
+++ HelloWorld.c
@@ -2,7 +2,22 @@
 
 int main() {
 
+    /*
+    This is
+    a multi-line comment
+    */
+    /*
+    \n = new line
+    \t = tab (4 spaces)
+    to place quotes inside a print statement use \", u can do the same with ' or \ 
+    the above are escape sequences
+    */
     printf("Hello, Wold!"); //this is a comment
     printf("I am learning\n");
+    printf("I\n am a Programming Language\n");
+    printf("1\t2\t3\t4\n");
+    printf("this is an \"example\" of quotes inside a print statment");
+
+
     return 0; //if code = 0 then program ran successfully, if not then there was an error
     }
